a212fc478a3bba3b29e9510e3eae01cdfeb0951e
amir-mln <devcareer.me@gmail.com>
2022-01-09 19:55:24 +0330

    redoing `Medium` exercises

--- AlgoExpert/2. Medium/10. Min Height BST/index.js
+++ AlgoExpert/2. Medium/10. Min Height BST/index.js
@@ -1,43 +1,18 @@
 // my solution
-// does not work right
-function BST(val) {
-  this.value = val;
-  this.left;
-  this.right;
+function INSERT(val) {
+  // the question itself should give us this function based on video explanation
 }
 
-function minHeightBst(arr, daddy) {
-  if (arr.length <= 1) return;
-  const left = 0;
-  const right = arr.length;
-  const headIndex = Math.floor((left + right) / 2);
-  const headLIndex = Math.floor((headIndex + left) / 2);
-  const headRIndex = Math.floor((headIndex + right) / 2);
+function minHeightBst(arr, lIndex = 0, rIndex = arr.length - 1) {
+  if (lIndex > rIndex) return;
+  const targetIndex = Math.floor((rIndex + lIndex) / 2);
+  INSERT(arr[targetIndex]);
+  // for the left branch
+  minHeightBst(arr, lIndex, targetIndex - 1);
+  // for the right branch
+  minHeightBst(arr, targetIndex + 1, rIndex);
 
-  if (!daddy) daddy = new BST(arr[headIndex]);
-
-  if (arr[headLIndex] !== undefined && headLIndex !== headIndex)
-    daddy.left = new BST(arr[headLIndex]);
-
-  if (arr[headRIndex] !== undefined && headRIndex !== headIndex)
-    daddy.right = new BST(arr[headRIndex]);
-
-  minHeightBst(arr.slice(left, headIndex), daddy.left);
-  minHeightBst(arr.slice(headIndex, right), daddy.right);
-
-  return daddy;
-}
-minHeightBst([0, 1, 2, 3, 4, 5, 6, 7, 8], 0, 8);
-
-// AlgoExpert's solution
-// O(n) time | O(n) space - where n is the length of the array
-function minHeightBst(array, startIdx, endIdx) {
-  if (endIdx < startIdx) return null;
-  const midIdx = Math.floor((startIdx + endIdx) / 2);
-  const bst = new BST(array[midIdx]);
-  bst.left = minHeightBst(array, startIdx, midIdx - 1);
-  bst.right = minHeightBst(array, midIdx + 1, endIdx);
-  return bst;
+  return targetIndex;
 }
 
-minHeightBst([0, 1, 2, 3, 4, 5, 6, 7, 8], 0, 8);
+minHeightBst([1, 2, 5, 7, 10, 13, 14, 15, 22], 0, 8);
